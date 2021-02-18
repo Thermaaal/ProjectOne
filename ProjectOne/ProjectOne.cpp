a201a4b1d1a7b1969eeeaef04fa151b83adada15
thermaaal <Thermal282mat>
2021-02-18 19:07:23 +0300

    Добавьте файлы проекта.

---
+++ ProjectOne/ProjectOne.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int a, b;
+    cout << "Введите чич" << endl;
+    cin >> a;
+    cout << "Введите второй чич" << endl;
+    cin >> b;
+    
+    while (a != b) {
+        if (a > b) {
+            a -= b;
+        } 
+        else {
+            b -= a;
+        }
+    }
+    cout << a << endl;
+
+    return 0;
+}
